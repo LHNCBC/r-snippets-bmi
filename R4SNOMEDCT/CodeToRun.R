0d2483b667e11f885f7b87ea61ef1d18d5933931
Vojtech Huser <vojtech.huser@gmail.com>
2018-08-03 12:26:57 -0400

    u

--- R4SNOMEDCT/CodeToRun.R
+++ R4SNOMEDCT/CodeToRun.R
@@ -1,12 +1,45 @@
 library(tidyverse)
-load('snomedct.rda')
+export_path<-'n:/snomed/'
 
-#works only for the most recent release
+load(file.path(export_path,'snomedct.rda'))
+
+
+
+#-------------------definition of functions -------------------------
+
+
+#' works only for the most recent release
 getNewConcepts <- function(release){
   
   c2<-c %>% dplyr::filter(active==1,effectiveTime==release)
-  c2
-  
+  names(c2)
+  names(d2)
+  c2 %>% left_join(d2,by=c('id'='conceptId'))
 }
-  
-#getNewConcepts('20180131')
+
+
+
+sct_search_text <-function(pattern){
+  d2 %>% dplyr::filter(grepl(pattern=pattern,term,ignore.case = TRUE))
+}
+
+sct_search_conceptId <-function(whichConceptId){
+  d2 %>% dplyr::filter(conceptId==whichConceptId)
+}
+
+
+
+
+#--------------------end of funcion definitions --------------------------------------------
+
+
+
+
+
+
+
+#try some of these commands
+
+#release='20180731'  
+#aa<-getNewConcepts(release)
+#aa %>% count(tag) %>% arrange(desc(n))
